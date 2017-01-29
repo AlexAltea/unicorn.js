e3c61748e17f409a152d88862a10d6e35099a5cb
Alexandro Sanchez Bach <alexandro@phi.nz>
2017-01-29 23:14:02 +0100

    Updated demo panes and added toolbars

--- demos/demo.js
+++ demos/demo.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    Split(['#a', '#b'], {
-      gutterSize: 8,
-      sizes: [65, 35],
-      cursor: 'col-resize'
-    });
-    Split(['#c', '#d'], {
-      direction: 'vertical',
-      sizes: [65, 35],
-      gutterSize: 8,
-      cursor: 'row-resize'
-    });
-    Split(['#e', '#f'], {
-      direction: 'vertical',
-      sizes: [65, 35],
-      gutterSize: 8,
-      cursor: 'row-resize'
-    });
+  Split(['#pane-l', '#pane-r'], {
+    gutterSize: 8,
+    sizes: [65, 35],
+    cursor: 'col-resize'
+  });
+  Split(['#pane-lt', '#pane-lb'], {
+    direction: 'vertical',
+    sizes: [65, 35],
+    gutterSize: 8,
+    cursor: 'row-resize'
+  });
+  Split(['#pane-rt', '#pane-rb'], {
+    direction: 'vertical',
+    sizes: [65, 35],
+    gutterSize: 8,
+    cursor: 'row-resize'
+  });
 });
